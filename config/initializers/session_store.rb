55862bb69d7e012caa06f78d3fd5c1b2d7e1c523
Nastya <zalesova98@mail.ru>
2020-05-17 02:42:35 +0300

    session fix

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env === 'production'
-  Rails.application.config.session_store :cookie_store, key: '_platform', domain: 'your-frontend-domain'
+  Rails.application.config.session_store :cookie_store, key: '_platform', domain: 'up-platform.herokuapp.com', expire_after: 5.minutes
 else
   Rails.application.config.session_store :cookie_store, key: '_platform', expire_after: 5.minutes
 end
